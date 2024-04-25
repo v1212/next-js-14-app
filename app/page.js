37d82a3d0c8be8d91773025e161d8d9a96eec124
annina-k <37171540+annina-k@users.noreply.github.com>
2024-04-25 17:50:51 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -61,7 +61,7 @@ export default function Home() {
           <h2>
             Learn <span>-&gt;</span>
           </h2>
-          <p>Learn about Static Web Apps Next.js in an interactive course with&nbsp;quizzes!</p>
+          <p>Learn about Static Web Apps Next.js in anqwaesrdtfgfdfsa interactive course with&nbsp;quizzes!</p>
         </a>
 
         <a
