62b9e166f265ae3af55702db42de54aa77b15bed
annina-k <37171540+annina-k@users.noreply.github.com>
2024-04-29 17:57:21 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get STARTED by editing&nbsp;
+          This is an update;
           <code className={styles.code}>app/page.js</code>
         </p>
         <div>
